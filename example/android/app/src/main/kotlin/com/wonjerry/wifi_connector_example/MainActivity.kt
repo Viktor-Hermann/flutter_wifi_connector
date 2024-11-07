8bd8b1d3da87577865b3c868e05ddb12bf6fceef
Viktor Hermann <v.hermann.it@gmail.com>
2024-11-07 22:01:49 +0200

    Update project and cleanup

--- example/android/app/src/main/kotlin/com/wonjerry/wifi_connector_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.wonjerry.wifi_connector_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {}
--- example/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:wifi_connector_example/main.dart';
-
-void main() {
-  testWidgets('Verify App', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-  });
-}
